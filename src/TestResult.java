c05f2be0e4cbf872d05b5eada997e3424a070f58
Raakkaani <lirodaki345@gmail.com>
2026-05-25 09:41:23 +0300

    зміни

--- src/TestResult.java
+++ src/TestResult.java
@@ -3,7 +3,7 @@ public class TestResult {
     public static void main(String[] args) {
         String[] word_test = {"Did", "deed", "civic", "eye", "dog", "cat"}; 
 
-         for (int i = 0; i < row_test.length; i++) {
+         for (int i = 0; i < word_test.length; i++) {
             boolean flase_true = Exercise.Calculate(row_test[i]);
 
             if(flase_true){
